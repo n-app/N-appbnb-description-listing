5b111d9de07e0e55825a1e41aeef16400e62cf30
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-06-07 19:07:25 -0700

    Changes from the last review made. Added ViewsAlert component.

--- client/src/js/components/Accomodations.jsx
+++ client/src/js/components/Accomodations.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Accomodation = (props) => {
-  console.log();
   return (
     <hgroup id="accomodations">
-      <img id="userIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon21.png" />
+      <img className="accomodationIcon" id="userIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon21.png" />
       <div id="" >{` ${props.data.numberOfGuests} ${props.data.numberOfGuests > 1 ? 'guests' : 'guest'} `}</div>
-      <img id="roomIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon19.png" />
+      <img className="accomodationIcon" id="roomIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon19.png" />
       <div id="" >{` ${props.data.numberOfRooms}  ${props.data.numberOfRooms > 1 ? 'bedrooms' : 'bedroom'} `}</div>
-      <img id="bedIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon15.png" />
+      <img className="accomodationIcon" id="bedIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon15.png" />
       <div id="" >{` ${props.data.numberBeds} ${props.data.numberBeds > 1 ? 'beds' : 'bed'} `}</div>
-      <img id="bathIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon13.png" />
+      <img className="accomodationIcon" id="bathIcon" alt="" src="https://s3-us-west-1.amazonaws.com/napbnb/icon13.png" />
       <div id="" >{` ${props.data.numberOfBaths} ${props.data.numberOfBaths > 1 ? 'bathrooms' : 'bathroom'}`}</div>
     </hgroup>
   );
--- client/src/js/components/Header.jsx
+++ client/src/js/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = (props) => {
   return (
     <hgroup id="listingHeader">
-      <div id="headerPropertyInfo">
-        <div id="listingTitle" >{props.data.title}</div>
-        <div id="listingLocation" >{props.data.location}</div>
+      <div className="headerPropertyInfo">
+        <div className="listingTitle" >{props.data.title}</div>
+        <div className="listingLocation" >{props.data.location}</div>
       </div>
-      <div id="headerOwnerInfo">
-        <img id="ownerImage" alt="home owner" src={props.data.ownerPicture_url.slice(1, 48).concat(props.data.ownerPicture_url.slice(49))} />
-        <div id="ownerName" >{props.data.owner}</div>
+      <div className="headerOwnerInfo">
+        <img className="ownerImage" alt="home owner" src={props.data.ownerPicture_url.slice(1, 48).concat(props.data.ownerPicture_url.slice(49))} />
+        <div className="ownerName" >{props.data.owner}</div>
       </div>
     </hgroup>
   );
