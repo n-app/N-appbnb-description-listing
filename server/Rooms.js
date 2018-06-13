4cfb1a74f380cd6b38baae9c7ffe7070aa4e003a
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-06-13 12:02:32 -0700

    Updated server headers and addded css font-family

--- server/Rooms.js
+++ server/Rooms.js
@@ -9,4 +9,5 @@ router.get('/:id', (req, res) => {
   });
 });
 
+
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -7,6 +7,23 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(express.static('../public'));
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Mehods',
+      'GET, POST, PUT, PATCH, DELETE',
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/rooms', roomsRoutes);
 
